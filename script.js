3cae6c656a6c2350d3d1d875ebf7443e27e77e2e
DanielRP <101836194+DanielRP14@users.noreply.github.com>
2022-03-18 11:12:53 -0300

    adicionei os arquivos do certificard

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
+
+//DESAFIOS:
+//1) Terminar todo o design que está no figma
+//2) Adicionar suas redes sociais de forma que quem clique consiga abrí-las
